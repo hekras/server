7ca1bbbd750d026e8c83e288e7d90d0393c55452
henryk_krasuski <hekras@gmail.com>
2019-06-07 05:40:34 +0200

    ecv added

---
+++ ecv.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+                font-family: sans-serif;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?php
+    include 'ecv-server.php';
+        ?>
+    </body>
+</html>
